835c11de9e0ffdee46268a788a1f4eff77fb390f
Kaviyarasan Tom <kaviyarasantom3@gmail.com>
2025-08-04 09:29:39 +0000

    video erase to that place replace a videoEnnavale Adi Ennavale Full Vide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <div className="relative mt-8 overflow-hidden rounded-xl" style={{ paddingTop: '56.25%' }}>
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/ApuD24a_b2k?autoplay=1&mute=1&loop=1&playlist=ApuD24a_b2k&controls=0"
+              src="https://www.youtube.com/embed/l-Iu44s3g28?autoplay=1&mute=1&loop=1&playlist=l-Iu44s3g28&controls=0"
               title="Project Video"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
