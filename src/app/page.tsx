02512a70f5ef8f60a98d4774a0dbb58718393d13
Kaviyarasan Tom <kaviyarasantom3@gmail.com>
2025-08-13 05:23:00 +0000

    background color set in this colour (home)  https://encrypted-tbn0.gstat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,6 +97,12 @@ export default function Home() {
   return (
     <div
       className="bg-background text-foreground"
+      style={{
+        backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7hapyllL4jF1UyHiHs__3OyYFn4ynZE1fI3Et7gdSYFmbnQqdzHFwCHWtqDhGA1zwfaY&usqp=CAU')`,
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
+      }}
     >
       <div className="flex flex-col min-w-0 w-full bg-black/50">
         {/* Hero Section */}
