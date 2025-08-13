b33e1bb754e362401c3adb39a09cc82513af9883
Kaviyarasan Tom <kaviyarasantom3@gmail.com>
2025-08-13 08:35:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -38,7 +37,7 @@ export function Footer() {
     }, []);
 
   return (
-    <footer className="bg-background border-t border-border">
+    <footer className="bg-black border-t border-border">
       <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           
